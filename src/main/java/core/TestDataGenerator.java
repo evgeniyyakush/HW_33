7abf9711f78bbcd0e2d8210d68fc54ef3b06abd3
Evgeniy Yakush <evgeniy.yakush@gmail.com>
2020-04-16 14:00:31 -0700

    Initial commit

---
+++ src/main/java/core/TestDataGenerator.java
@@ -0,0 +1,13 @@
+package core;
+
+public class TestDataGenerator {
+	int length = 0, result = 0;
+	static int max =101;
+
+	public static void main(String[] args) {
+		for (int i = 2; i <= max; i++) {
+			System.out.println("{" + i + ", " + PrimeNumberChecker.validate(i) + "}" + (i < max ? "," : ""));
+			//System.out.println("{" + i + ",  false  }" + (i < max ? "," : ""));
+		}
+	}
+}
